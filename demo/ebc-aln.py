bc5f7b630f474c1542323c758744134de864d1f5
David J. H. Shih <djh.shih@gmail.com>
2026-05-20 23:04:26 +0800

    Add EBC-ALN.

---
+++ demo/ebc-aln.py
@@ -0,0 +1,151 @@
+# Management of ALN involvement in early breast cancer
+# Figure 3, Loibl et al., 2024, https://doi.org/10.1016/j.annonc.2023.11.016
+
+# TODO we need a stopping condition and an action list!
+
+# TODO distinguish between different node status: c vs. i vs. p
+
+# NOTE  not None == True,
+#  so we need to check binary variables against False and True explicitly
+
+# NOTE  use n_status == "pNX" rather than n_status != "pN+"
+#       allows the walk to stop because condition of neither branch is met
+
+
+import os, sys
+
+from dataclasses import dataclass
+
+sys.path.append(os.path.abspath('..'))
+import dgraph.graph as dg
+from dgraph.graph import Node
+
+@dataclass
+class Data:
+    neoadjuvant: bool = None
+    # c: clinical (palpation); i: imaging; p: pathological
+    n_status: str = None
+    sln_positive: bool = None
+    acosog_z0011: bool = None
+    amaros: bool = None
+    positive_nodes: int = None
+    # y: after neoadjuvant
+    n_status_residual: str = None    # ycN0, ypN0, ycN+, ypN+
+    tad_positive: bool = None
+
+rt_ba = Node("RT (basis axilla) [II, B]")
+rt_a = Node("RT (axilla) [II, B]")
+alnd = Node("ALND [II, A]")
+
+sln_neg = Node("SLN-",
+    condition = lambda x: x.sln_positive == False,
+    children = [
+        Node("No further locoregional treatment")
+    ],
+)
+
+bottom_branches = [
+    Node("ACOSOG-Z0011 criteria met",
+        condition = lambda x: x.acosog_z0011,
+        children = [ rt_ba ]
+    ),
+    Node("AMAROS critiera met",
+        condition = lambda x: x.amaros,
+        children = [ rt_a, alnd ]
+    ),
+    Node("ACOSOG-Z0011 criteria not met or >2 positive LNs",
+        condition = lambda x: x.acosog_z0011 == False and x.positive_nodes > 2,
+        children = [ alnd ]
+    ),
+]
+
+sln_pos = Node("SLN+",
+    condition = lambda x: x.sln_positive == True,
+    children = bottom_branches,
+)
+
+slnb = Node("SLNB [I, A]",
+    children = [sln_neg, sln_pos]
+)
+
+biopsy = Node("Biopsy",
+    children = [
+        Node("pNX",
+            condition = lambda x: x.n_status == "pNX",
+            children = [ slnb ]
+        ),
+        Node("pN+",
+            condition = lambda x: x.n_status == "pN+",
+            children = bottom_branches
+        )
+    ]
+)
+
+surgery_indicated = Node("primary surgery indicated",
+    condition = lambda x: not x.neoadjuvant,
+    children = [
+        Node("cN0/iN0",
+            condition = lambda x: x.n_status in ("cN0", "iN0"),
+            children = [ slnb ]
+        ),
+        Node("cN+/iN+",
+            condition = lambda x: x.n_status in ("cN+", "iN+"),
+            children = [ biopsy ]
+        ),
+    ]
+)
+
+alnd = Node("ALND (or RT) of regional LNs [II, B]")
+
+neoadjuvant_therapies = [ Node("Follow Figures 4-7 for neoadjuvant therapy",
+    children = [
+        Node("ycN0/ypN0 after neoadjuvant ChT",
+            condition = lambda x: x.n_status_residual in ("ycN0", "ypN0"),
+            children = [
+                Node("SLN- or TAD-",
+                    condition = lambda x: not sln_positive or not x.tad_positive,
+                    children = [
+                        Node("Consider RT if pN+ at primary diagnosis [II, B]")
+                    ]
+                ),
+                Node("SLN+ or TAD+",
+                    condition = lambda x: sln_positive or x.tad_positive,
+                    children = [ alnd ]
+                ),
+            ]
+        ),
+        Node("ycN+/ypN+ after neoadjuvant ChT",
+            condition = lambda x: x.n_status_residual in ("ycN+", "ypN+"),
+            children = [ alnd ]
+        ),
+    ]
+) ]
+
+neoadjuvant_indicated = Node("PST indicated",
+    condition = lambda x: x.neoadjuvant,
+    children = [
+        Node("cN0/pN0 at primary diagnosis",
+            condition = lambda x: x.n_status in ("cN0", "pN0"),
+            children = neoadjuvant_therapies
+        ),
+        Node("cN+/pN+ at primary diagnosis",
+            condition = lambda x: x.n_status in ("cN+", "pN+"),
+            children = neoadjuvant_therapies
+        ),
+    ]
+)
+
+graph = Node(
+    "EBC-ALN",
+    children = [surgery_indicated, neoadjuvant_indicated]
+)
+
+examples = [
+    Data(neoadjuvant=False, n_status = "iN+", positive_nodes=3),
+    Data(neoadjuvant=False, n_status = "cN+", positive_nodes=1),
+    Data(neoadjuvant=True, n_status = "cN0"),
+]
+
+for x in examples:
+    print(dg.walk(graph, x))
+
