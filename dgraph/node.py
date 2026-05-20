4cdaf0538d25edbaec1e2354718965794d18dfff
David J. H. Shih <djh.shih@gmail.com>
2026-05-20 19:57:53 +0800

    Initial commit.

---
+++ dgraph/node.py
@@ -0,0 +1,90 @@
+from dataclasses import dataclass, field, replace
+from typing import Any, Callable
+
+@dataclass
+class Node:
+    label: str
+    condition: Callable[[Any], bool] = lambda x: True
+    children: list["Node"] = field(default_factory=list)
+
+def walk(node: Node, data):
+    if not node.condition(data):
+        return
+    yield node.label
+    for c in node.children:
+        yield from walk(c, data)
+
+
+# ---
+
+@dataclass
+class Data:
+    hr_status: bool
+    her2_status: bool
+    t_status: str
+    n_status: str
+    m_status: str = "M0"
+    postmenopausal: bool = False
+    receiving_ofs: bool = False
+
+surgery = Node("Primary surgery +/- RT")
+neoadjuvant = Node("Neoadjuvant therapy")
+systemic = Node("Systemic treatment")
+
+graph = Node('EBC',
+    children = [
+        Node("HR+",
+            condition = lambda x: x.hr_status,
+            children = [Node("ET [I, A]")]
+        ),
+        Node("Premenopausal patients receiving OFS and postmenopausal patients",
+            condition = lambda x: x.postmenopausal or (not x.postmenopausal and x.receiving_ofs),
+            children = [Node("Adjuvant bisphosphonates [I, A]")]
+        ),
+        Node("HR+/HER-",
+             condition = lambda x: x.hr_status and not x.her2_status,
+             children = [ replace(neoadjuvant, children = [ replace(surgery, children = [systemic]) ]) ],
+        ),
+        Node("HER2+",
+            condition = lambda x: x.her2_status,
+            children = [
+                Node("cT1 N0",
+                    condition = lambda x: x.t_status == "T1" and x.n_status == "N0",
+                    children = [ replace(surgery, children = [systemic]) ],
+                ),
+                Node(">=cT2 or cN+",
+                    condition = lambda x: x.t_status != "T1" or x.n_status == "N+",
+                    children = [ replace(neoadjuvant, children = [ replace(surgery, children = [systemic]) ]) ],
+                ),
+            ],
+        ),
+        Node("TNBC",
+            condition = lambda x: not x.hr_status and not x.her2_status,
+            children = [
+                Node("cT1a or cT1b N0",
+                    condition = lambda x: (x.t_status == "T1a" or x.t_status == "T1b") and x.n_status == "N0",
+                    children = [ replace(surgery, children = [systemic]) ],
+                ),
+                Node("cT1c-4 or N+",
+                    condition = lambda x: (x.t_status in ["T1c", "T2", "T3", "T4"]) or x.n_status == "N+",
+                    children = [ replace(neoadjuvant, children = [ replace(surgery, children = [systemic]) ]) ],
+                ),
+            ]
+        ),
+    ]
+)
+
+graph
+
+x = Data(her2_status=True, hr_status=False, t_status = "T1", n_status = "N0")
+list(walk(graph, x))
+
+x = Data(her2_status=False, hr_status=False, t_status = "T1a", n_status = "N0")
+list(walk(graph, x))
+
+x = Data(her2_status=False, hr_status=False, t_status = "T1a", n_status = "N+")
+list(walk(graph, x))
+
+x = Data(her2_status=True, hr_status=False, t_status = "T1", n_status = "N+")
+list(walk(graph, x))
+
