9f8261898288f7124dffad1b17dc014398c00327
David J. H. Shih <djh.shih@gmail.com>
2026-05-22 16:10:12 +0800

    Add demo script.

---
+++ demo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 
+
+echo "python -m demo.ebc.ebc-dx"
+python -m demo.ebc.ebc-dx
+echo
+
+echo "python -m demo.ebc.ebc"
+python -m demo.ebc.ebc
+echo
+
+echo "python -m demo.ebc.ebc-aln"
+python -m demo.ebc.ebc-aln
+echo
+
